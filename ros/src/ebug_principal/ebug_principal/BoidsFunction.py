3337e6290353140f82b3a060722808097219b6ec
Mark-Diedericks <mark.diedericks830@gmail.com>
2024-04-18 16:58:01 +1000

    Added boid view distance

--- ros/src/ebug_principal/ebug_principal/BoidsFunction.py
+++ ros/src/ebug_principal/ebug_principal/BoidsFunction.py
@@ -13,29 +13,13 @@ LOWER_HEIGHT = int(ARENA_HEIGHT * BUFFER_SPACE)
 UPPER_HEIGHT = ARENA_HEIGHT - LOWER_HEIGHT
 
 # Boid parameters
-MAX_SPEED = 50              # mm /s 
+MAX_FORWARD_SPEED = 50      # mm /s 
+MAX_ANGULAR_SPEED = 50      # degrees /s 
 SEPARATION_DISTANCE = 100   # mm
-ALIGNMENT_FACTOR = 0.01
-COHESION_FACTOR = 0.01
-SEPARATION_FACTOR = 0.01
-
-# Function to calculate distance between two boids
-def distance(pose1, pose2):
-    return math.sqrt(
-            (pose1.position.x - pose2.position.x) ** 2 
-          + (pose1.position.y - pose2.position.y) ** 2)
-
-# Function to calculate angle between two boids
-def angle_between(pose1, other_x, other_y):
-    return math.atan2(other_y - pose1.position.y, other_x - pose1.position.x)
-
-# Function to get the Euler Z angle of a boid
-def angle(pose):
-    roll, pitch, yaw = euler_from_quaternion(pose.orientation.x, pose.orientation.y, pose.orientation.z, pose.orientation.w)
-    return yaw
-
-def sign(num):
-    return -1 if num < 0 else 1
+VIEW_DISTANCE = 500         # mm
+ALIGNMENT_FACTOR = 0.1
+COHESION_FACTOR = 0.1
+SEPARATION_FACTOR = 0.1
 
 # Function to implement Boid rules
 def next(main_boid, other_boids):
@@ -47,19 +31,21 @@ def next(main_boid, other_boids):
     count_separation = 0
     
     for other_boid in other_boids:
-        cohesion[0] += other_boid.position.x # find average x position of all other boids
-        cohesion[1] += other_boid.position.y # find average y position of all other boids
-        count_cohesion += 1
-        
-        alignment[0] += math.cos(angle(other_boid))  # TODO get angle??? other_boid.orientation is a Quaternion
-        alignment[1] += math.sin(angle(other_boid))  # TODO get angle??? other_boid.orientation is a Quaternion
-        count_alignment += 1
+        if distance(main_boid, other_boid) <= VIEW_DISTANCE:
+            cohesion[0] += other_boid.position.x
+            cohesion[1] += other_boid.position.y
+            count_cohesion += 1
             
-        if distance(main_boid, other_boid) < SEPARATION_DISTANCE:
-            separation[0] += (main_boid.position.x - other_boid.position.x)
-            separation[1] += (main_boid.position.y - other_boid.position.y)
-            count_separation += 1
+            alignment[0] += math.cos(angle(other_boid))
+            alignment[1] += math.sin(angle(other_boid))
+            count_alignment += 1
+                
+            if distance(main_boid, other_boid) < SEPARATION_DISTANCE:
+                separation[0] += (main_boid.position.x - other_boid.position.x)
+                separation[1] += (main_boid.position.y - other_boid.position.y)
+                count_separation += 1
     
+
     resultant_angle = angle(main_boid)
     if count_cohesion > 0:
         cohesion[0] /= count_cohesion
@@ -81,7 +67,7 @@ def next(main_boid, other_boids):
 
 
     # TODO set desired linear velocity (this should just be constant ??)
-    linear_velocity = MAX_SPEED
+    linear_velocity = MAX_FORWARD_SPEED
 
     x_comp = math.cos(resultant_angle) # negative is towards left, positive is towards right
     y_comp = math.sin(resultant_angle) # negative is towards bottom, positive is towards up
@@ -97,31 +83,36 @@ def next(main_boid, other_boids):
     
     # TODO compare resultant_angle with angle(main_boid) to determine angular velocity
     # Maybe keep a constant linear velocity and only worry about signing it
-    angular_velocity = 1 * sign(resultant_angle - angle(main_boid))
+    angular_velocity = MAX_ANGULAR_SPEED * sign(resultant_angle - angle(main_boid))
 
     led_colour = (255, 0, 0)
     # Return (Linear Velocity Forward, Angular Velocity Yaw)
     return (linear_velocity, angular_velocity, led_colour)
 
 
-def euler_from_quaternion(x, y, z, w):
-    """
-    Convert a quaternion into euler angles (roll, pitch, yaw)
-    roll is rotation around x in radians (counterclockwise)
-    pitch is rotation around y in radians (counterclockwise)
-    yaw is rotation around z in radians (counterclockwise)
-    """
-    t0 = +2.0 * (w * x + y * z)
-    t1 = +1.0 - 2.0 * (x * x + y * y)
-    roll_x = math.atan2(t0, t1)
+# Function to calculate distance between two boids
+def distance(pose1, pose2):
+    return math.sqrt(
+            ((pose1.position.x - pose2.position.x) ** 2)
+          + ((pose1.position.y - pose2.position.y) ** 2) )
 
-    t2 = +2.0 * (w * y - z * x)
-    t2 = +1.0 if t2 > +1.0 else t2
-    t2 = -1.0 if t2 < -1.0 else t2
-    pitch_y = math.asin(t2)
 
+# Function to calculate angle between two boids
+def angle_between(pose1, other_x, other_y):
+    return math.atan2(other_y - pose1.position.y, other_x - pose1.position.x)
+
+
+# Function to get the Euler Z angle of a boid
+def angle(pose):
+    x, y, z, w = pose.orientation.x, pose.orientation.y, pose.orientation.z, pose.orientation.w
+
+    # Calculate yaw/z component from quaternion
     t3 = +2.0 * (w * z + x * y)
     t4 = +1.0 - 2.0 * (y * y + z * z)
     yaw_z = math.atan2(t3, t4)
 
-    return roll_x, pitch_y, yaw_z # in radians
+    return yaw_z
+
+
+def sign(num):
+    return -1 if num < 0 else 1
