509dd16b050dd672a51a44d37afe28f8c1711f94
Mark-Diedericks <mark.diedericks830@gmail.com>
2024-04-19 21:19:24 +1000

    Cache covariance matrix

--- ros/src/ebug_agent/ebug_agent/TransformConverter.py
+++ ros/src/ebug_agent/ebug_agent/TransformConverter.py
@@ -31,6 +31,7 @@ class TransformConverter(Node):
         self.publisher = self.create_publisher(PoseWithCovarianceStamped, 'pose', 100)
 
         self.cameras = [self.get_camera(i) for i in range(4)]
+        self.covariance = mat6diag(1e-3)
     
     def get_camera(self, cam_id):
         t = Transform()
@@ -72,7 +73,7 @@ class TransformConverter(Node):
             msg.pose.pose.orientation.z = tag_robot.rotation.z
             msg.pose.pose.orientation.w = tag_robot.rotation.w
 
-            msg.pose.covariance = mat6diag(self.alpha)
+            msg.pose.covariance = self.covariance
 
             self.publisher.publish(msg)
 
