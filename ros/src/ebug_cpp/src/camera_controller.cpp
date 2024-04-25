8722c64be47e9ed1b46742aa00e6826e252f0d85
Mark-Diedericks <mark.diedericks830@gmail.com>
2024-04-25 23:13:20 +1000

    Scope synchronizers

--- ros/src/ebug_cpp/src/camera_controller.cpp
+++ ros/src/ebug_cpp/src/camera_controller.cpp
@@ -21,21 +21,24 @@ using namespace std::chrono_literals;
 
 class CameraController : public rclcpp::Node
 {
+    typedef message_filters::sync_policies::ApproximateTime<sensor_msgs::msg::Image, sensor_msgs::msg::CameraInfo> approximate_policy;
+
     private:
         rclcpp::Publisher<sensor_msgs::msg::CameraInfo>::SharedPtr m_CameraInfoPublisher;
         rclcpp::Publisher<sensor_msgs::msg::CompressedImage>::SharedPtr m_CompressedImagePublisher;
 
+        std::vector<std::string> m_Cameras;
+        std::vector<std::shared_ptr<message_filters::Synchronizer<approximate_policy>>> m_Synchronizers;
+
     public:
         CameraController() : Node("CameraController")
         {
-            std::vector<std::string> cameras = this->declare_parameter<std::vector<std::string>>
-                ("cameras", std::vector<std::string>( { "cam_0" } ));
+            m_Cameras = this->declare_parameter<std::vector<std::string>>("cameras", std::vector<std::string>( { "cam_0" } ));
 
             m_CompressedImagePublisher = this->create_publisher<sensor_msgs::msg::CompressedImage>("image_raw/compressed", 10);
             m_CameraInfoPublisher = this->create_publisher<sensor_msgs::msg::CameraInfo>("camera_info", 10);
 
-            typedef message_filters::sync_policies::ApproximateTime<sensor_msgs::msg::Image, sensor_msgs::msg::CameraInfo> approximate_policy;
-            for (const std::string& cam_id : cameras) 
+            for (auto &cam_id : m_Cameras) 
             {
                 message_filters::Subscriber<sensor_msgs::msg::Image> image(this, cam_id + "/image_raw");
                 message_filters::Subscriber<sensor_msgs::msg::CameraInfo> cinfo(this, cam_id + "/camera_info");
@@ -45,12 +48,15 @@ class CameraController : public rclcpp::Node
                 sync->setMaxIntervalDuration(rclcpp::Duration(0,100000000)); // 0.1 Second interval
                 sync->registerCallback(std::bind(&CameraController::camera_callback, this, std::placeholders::_1, std::placeholders::_2));
 
-                RCLCPP_INFO(this->get_logger(), "Connected USB Camera (ID: %s) '%s' '%s'", cam_id, cam_id + "/image_raw", cam_id + "/camera_info");
+                m_Synchronizers.push_back(sync);
+
+                RCLCPP_INFO(this->get_logger(), "Connected USB Camera (ID: %s) '%s' '%s'", 
+                    cam_id.c_str(), (cam_id + "/image_raw").c_str(), (cam_id + "/camera_info").c_str());
             }
         }
 
     private:
-        void camera_callback(const sensor_msgs::msg::Image::ConstSharedPtr& image, const sensor_msgs::msg::CameraInfo::ConstSharedPtr& cinfo)
+        void camera_callback(const sensor_msgs::msg::Image::ConstSharedPtr& image, const sensor_msgs::msg::CameraInfo::ConstSharedPtr& cinfo) const
         {
             RCLCPP_INFO(this->get_logger(), "Received Image & CameraInfo");
 
