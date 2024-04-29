223e8289befa2010ac54159053fe45c99298870d
Mark-Diedericks <mark.diedericks830@gmail.com>
2024-04-29 19:49:03 +1000

    Write as composable

--- ros/src/ebug_cpp/src/byte_rectifier.cpp
+++ ros/src/ebug_cpp/src/byte_rectifier.cpp
@@ -11,42 +11,45 @@
 
 using namespace std::chrono_literals;
 
-/* This example creates a subclass of Node and uses std::bind() to register a
-* member function as a callback from the timer. */
+#include <rclcpp_components/register_node_macro.hpp>
+RCLCPP_COMPONENTS_REGISTER_NODE(ebug::ByteRectifier)
 
-class ByteRectifier : public rclcpp::Node
+
+namespace ebug
 {
-    private:
-        rclcpp::Publisher<sensor_msgs::msg::CompressedImage>::SharedPtr m_CompressedImagePublisher;
-        rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr m_ImageSubscription;
+    class ByteRectifier : public rclcpp::Node
+    {
+        private:
+            rclcpp::Publisher<sensor_msgs::msg::CompressedImage>::SharedPtr m_CompressedImagePublisher;
+            rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr m_ImageSubscription;
 
-    public:
-        ByteRectifier() : Node("ByteRectifier")
-        {
-            m_CompressedImagePublisher = this->create_publisher<sensor_msgs::msg::CompressedImage>("image_compressed", 10);
+        public:
+            ByteRectifier(const rclcpp::NodeOptions& options) : Node("ByteRectifier", options)
+            {
+                m_CompressedImagePublisher = this->create_publisher<sensor_msgs::msg::CompressedImage>("image_compressed", 10);
 
-            m_ImageSubscription = this->create_subscription<sensor_msgs::msg::Image>("image_raw", 10, std::bind(&ByteRectifier::camera_callback, this, std::placeholders::_1));
-        }
+                m_ImageSubscription = this->create_subscription<sensor_msgs::msg::Image>("image_raw", 10, std::bind(&ByteRectifier::camera_callback, this, std::placeholders::_1));
+            }
 
-    private:
-        void camera_callback(const sensor_msgs::msg::Image::ConstSharedPtr& image) const
-        {
-            sensor_msgs::msg::CompressedImage cimage;
+        private:
+            void camera_callback(const sensor_msgs::msg::Image::ConstSharedPtr& image) const
+            {
+                sensor_msgs::msg::CompressedImage cimage;
 
-            cimage.header = image->header;
-            cimage.data = image->data;
+                cimage.header = image->header;
+                cimage.data = image->data;
 
-            cimage.format = "jpeg";
-            
-            m_CompressedImagePublisher->publish(cimage);
-        }
-};
-
+                cimage.format = "jpeg";
+                
+                m_CompressedImagePublisher->publish(cimage);
+            }
+    };
+}
 
 int main(int argc, char * argv[])
 {
   rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<ByteRectifier>());
+  rclcpp::spin(std::make_shared<ebug::ByteRectifier>());
   rclcpp::shutdown();
   return 0;
 }
--- ros/src/ebug_cpp/src/camera_controller.cpp
+++ ros/src/ebug_cpp/src/camera_controller.cpp
@@ -15,62 +15,66 @@
 
 using namespace std::chrono_literals;
 
-/* This example creates a subclass of Node and uses std::bind() to register a
-* member function as a callback from the timer. */
+#include <rclcpp_components/register_node_macro.hpp>
+RCLCPP_COMPONENTS_REGISTER_NODE(ebug::CameraController)
 
-class CameraController : public rclcpp::Node
+
+namespace ebug
 {
-    typedef message_filters::sync_policies::ApproximateTime<sensor_msgs::msg::Image, sensor_msgs::msg::CameraInfo> approximate_policy;
+    class CameraController : public rclcpp::Node
+    {
+        typedef message_filters::sync_policies::ApproximateTime<sensor_msgs::msg::Image, sensor_msgs::msg::CameraInfo> approximate_policy;
 
-    private:
-        rclcpp::Publisher<sensor_msgs::msg::CameraInfo>::SharedPtr m_CameraInfoPublisher;
-        rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr m_ImagePublisher;
+        private:
+            rclcpp::Publisher<sensor_msgs::msg::CameraInfo>::SharedPtr m_CameraInfoPublisher;
+            rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr m_ImagePublisher;
 
-        std::vector<std::string> m_Cameras;
-        std::vector<std::shared_ptr<message_filters::Subscriber<sensor_msgs::msg::Image>>> m_Images;
-        std::vector<std::shared_ptr<message_filters::Subscriber<sensor_msgs::msg::CameraInfo>>> m_CameraInfos;
-        std::vector<std::shared_ptr<message_filters::Synchronizer<approximate_policy>>> m_Synchronizers;
+            std::vector<std::string> m_Cameras;
+            std::vector<std::shared_ptr<message_filters::Subscriber<sensor_msgs::msg::Image>>> m_Images;
+            std::vector<std::shared_ptr<message_filters::Subscriber<sensor_msgs::msg::CameraInfo>>> m_CameraInfos;
+            std::vector<std::shared_ptr<message_filters::Synchronizer<approximate_policy>>> m_Synchronizers;
 
-    public:
-        CameraController() : Node("CameraController")
-        {
-            m_Cameras = this->declare_parameter<std::vector<std::string>>("cameras", std::vector<std::string>( { "cam_0" } ));
-
-            m_ImagePublisher = this->create_publisher<sensor_msgs::msg::Image>("image_rect", 10);
-            m_CameraInfoPublisher = this->create_publisher<sensor_msgs::msg::CameraInfo>("camera_info", 10);
-
-            for (auto &cam_id : m_Cameras) 
+        public:
+            CameraController(const rclcpp::NodeOptions& options) : Node("CameraController", options)
             {
-                auto image = std::make_shared<message_filters::Subscriber<sensor_msgs::msg::Image>>(this, cam_id + "/image_rect");
-                image->subscribe();
-                m_Images.push_back(image);
+                m_Cameras = this->declare_parameter<std::vector<std::string>>("cameras", std::vector<std::string>( { "cam_0" } ));
 
-                auto cinfo = std::make_shared<message_filters::Subscriber<sensor_msgs::msg::CameraInfo>>(this, cam_id + "/camera_info");
-                m_CameraInfos.push_back(cinfo);
-                cinfo->subscribe();
+                m_ImagePublisher = this->create_publisher<sensor_msgs::msg::Image>("image_rect", 10);
+                m_CameraInfoPublisher = this->create_publisher<sensor_msgs::msg::CameraInfo>("camera_info", 10);
 
-                auto sync = std::make_shared<message_filters::Synchronizer<approximate_policy>>(approximate_policy(10), *image, *cinfo);
-                m_Synchronizers.push_back(sync);
-                
-                sync->setMaxIntervalDuration(rclcpp::Duration(0,100000000)); // 0.1 Second interval
-                sync->registerCallback(std::bind(&CameraController::camera_callback, this, std::placeholders::_1, std::placeholders::_2));
+                for (auto &cam_id : m_Cameras) 
+                {
+                    auto image = std::make_shared<message_filters::Subscriber<sensor_msgs::msg::Image>>(this, cam_id + "/image_rect");
+                    image->subscribe();
+                    m_Images.push_back(image);
 
-                RCLCPP_INFO(this->get_logger(), "Connected USB Camera (ID: %s)", cam_id.c_str());
+                    auto cinfo = std::make_shared<message_filters::Subscriber<sensor_msgs::msg::CameraInfo>>(this, cam_id + "/camera_info");
+                    m_CameraInfos.push_back(cinfo);
+                    cinfo->subscribe();
+
+                    auto sync = std::make_shared<message_filters::Synchronizer<approximate_policy>>(approximate_policy(10), *image, *cinfo);
+                    m_Synchronizers.push_back(sync);
+                    
+                    sync->setMaxIntervalDuration(rclcpp::Duration(0,100000000)); // 0.1 Second interval
+                    sync->registerCallback(std::bind(&CameraController::camera_callback, this, std::placeholders::_1, std::placeholders::_2));
+
+                    RCLCPP_INFO(this->get_logger(), "Connected USB Camera (ID: %s)", cam_id.c_str());
+                }
             }
-        }
 
-    private:
-        void camera_callback(const sensor_msgs::msg::Image::ConstSharedPtr& image, const sensor_msgs::msg::CameraInfo::ConstSharedPtr& cinfo) const
-        {            
-            m_ImagePublisher->publish(*image);
-            m_CameraInfoPublisher->publish(*cinfo);
-        }
-};
+        private:
+            void camera_callback(const sensor_msgs::msg::Image::ConstSharedPtr& image, const sensor_msgs::msg::CameraInfo::ConstSharedPtr& cinfo) const
+            {            
+                m_ImagePublisher->publish(*image);
+                m_CameraInfoPublisher->publish(*cinfo);
+            }
+    };
+}
 
 int main(int argc, char * argv[])
 {
   rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<CameraController>());
+  rclcpp::spin(std::make_shared<ebug::CameraController>());
   rclcpp::shutdown();
   return 0;
 }
