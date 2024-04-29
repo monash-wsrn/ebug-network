d7d78ba1c5e05e959701995cee8785f5aedd5a83
Mark-Diedericks <mark.diedericks830@gmail.com>
2024-04-29 20:08:48 +1000

    Make composable

--- ros/src/ebug_client/launch/ebug_client.launch.py
+++ ros/src/ebug_client/launch/ebug_client.launch.py
@@ -1,8 +1,12 @@
 import os
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, TimerAction
-from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
+from launch_ros.descriptions import ComposableNode
+from launch_ros.actions import Node, ComposableNodeContainer, LoadComposableNodes
+
+from launch.actions import DeclareLaunchArgument, TimerAction, SetLaunchConfiguration
+from launch.conditions import LaunchConfigurationEquals, LaunchConfigurationNotEquals
+from launch.substitutions import LaunchConfiguration
 
 def generate_launch_description():
     ROBOT_ID = os.getenv('ROBOT_ID', "default")
@@ -11,22 +15,46 @@ def generate_launch_description():
     PKG_SHARE = FindPackageShare(package='ebug_client').find('ebug_client')
 
 
-    CAMERA_NODES = []
+    COMPOSABLE_NODES = []
     VIDX = 0    # 0, 2, 4, and 6
     for cam_id in CAMERA_IDS:
-        CAMERA_NODES.extend( create_camera_nodes(ROBOT_ID, cam_id, PKG_SHARE, f'/dev/video{VIDX}') )
+        COMPOSABLE_NODES.extend( create_camera_composable_nodes(ROBOT_ID, cam_id, PKG_SHARE, f'/dev/video{VIDX}') )
         VIDX += 2
         
         
-    CameraControllerNode = Node(
+    CameraControllerNode = ComposableNode(
         package = 'ebug_cpp',
-        executable = 'camera_controller',
+        plugin = 'ebug::CameraController',
         name = 'CameraController',
         namespace = ROBOT_ID,
         parameters=[
             {'cameras':     CAMERA_IDS  }
         ]
     )
+    COMPOSABLE_NODES.append(CameraControllerNode)
+
+    ComposablesContainer = ComposableNodeContainer(
+        condition=LaunchConfigurationEquals('ebug_container', ''),
+        package='rclcpp_components',
+        executable='component_container',
+        name='ebug_composables_container',
+        namespace='',
+        composable_node_descriptions=COMPOSABLE_NODES,
+    )
+
+    ComposablesLoader = LoadComposableNodes(
+        condition=LaunchConfigurationNotEquals('ebug_container', ''),
+        composable_node_descriptions=COMPOSABLE_NODES,
+        target_container=LaunchConfiguration('ebug_container'),
+    )
+
+    ComposablesConfig = SetLaunchConfiguration(
+        condition=LaunchConfigurationEquals('ebug_container', ''),
+        name='ebug_container',
+        value='ebug_composables_container'
+    )
+
+
 
     RobotControllerNode = Node(
         package = 'ebug_client',
@@ -41,23 +69,26 @@ def generate_launch_description():
             default_value='false',
             description='Use simulation (Gazebo) clock if true'),
 
-        *CAMERA_NODES,
+        ComposablesContainer,
+        ComposablesLoader,
+        ComposablesConfig,
         CameraControllerNode,
         TimerAction(period=5.0, actions=[RobotControllerNode]) # Apply delayed start to movement controller, allow initial localization
     ])
 
 
 
-def create_camera_nodes(ROBOT_ID, CAM_ID, PKG_SHARE, VIDEO_DEVICE):   
+def create_camera_composable_nodes(ROBOT_ID, CAM_ID, PKG_SHARE, VIDEO_DEVICE):   
     FRAME_RATE = 10.0
     WIDTH = 640
     HEIGHT = 480
     CAM_INFO = os.path.join(PKG_SHARE, f'calibration/{CAM_ID}.yaml') 
     NAMESPACE = f'{ROBOT_ID}/{CAM_ID}'
 
-    CameraNode = Node(
+    # https://github.com/ros-drivers/usb_cam/blob/ros2/CMakeLists.txt
+    CameraNode = ComposableNode(
         package = 'usb_cam',
-        executable = 'usb_cam_node_exe',
+        plugin = 'usb_cam::UsbCamNode',
         name = 'Camera',
         namespace = NAMESPACE,
 
@@ -74,17 +105,18 @@ def create_camera_nodes(ROBOT_ID, CAM_ID, PKG_SHARE, VIDEO_DEVICE):
     )
 
     # Correctly select raw camera bytes for compressed data stream
-    ByteRectifier = Node(
+    ByteRectifier = ComposableNode(
         package = 'ebug_cpp',
-        executable = 'byte_rectifier',
+        plugin = 'ebug::ByteRectifier',
         name = 'ByteRectifier',
         namespace = NAMESPACE,
     )
 
+    # https://github.com/ros-perception/image_common/blob/rolling/image_transport/CMakeLists.txt
     # Decompress jpeg format into raw image
-    MotionJPEGDecompress = Node(
+    MotionJPEGDecompress = ComposableNode(
         package = 'image_transport',
-        executable = 'republish',
+        plugin = 'image_transport::Republisher',
         name = 'DecompressMJPEG',
         namespace = NAMESPACE,
     
@@ -94,14 +126,15 @@ def create_camera_nodes(ROBOT_ID, CAM_ID, PKG_SHARE, VIDEO_DEVICE):
         ],
         remappings = [
             ('in/compressed', 'image_compressed'),
-            ('out', 'image_rect'), 
+            ('out', 'image_uncompressed'), 
         ]
     )
 
+    # https://github.com/ros-perception/image_pipeline/blob/rolling/image_proc/CMakeLists.txt
     # launch the image processing nodes (rectify camera distortion)
-    ImageProcNode = Node(
+    ImageProcNode = ComposableNode(
         package = 'image_proc',
-        executable = 'rectify_node',
+        plugin = 'image_proc::RectifyNode',
         name = 'RectifyColor',
         namespace = NAMESPACE,
 
@@ -110,4 +143,4 @@ def create_camera_nodes(ROBOT_ID, CAM_ID, PKG_SHARE, VIDEO_DEVICE):
         ]
     )
 
-    return [CameraNode, ByteRectifier, MotionJPEGDecompress ] #, ImageProcNode]
+    return [CameraNode, ByteRectifier, MotionJPEGDecompress, ImageProcNode]
