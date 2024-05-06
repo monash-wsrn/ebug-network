668e575ce541bd6ffbce8e4584e65707aba50133
Mark-Diedericks <mark.diedericks830@gmail.com>
2024-05-06 16:56:43 +1000

    Change Boids angular velocity limit

--- ros/src/ebug/ebug/util/BoidsFunction.py
+++ ros/src/ebug/ebug/util/BoidsFunction.py
@@ -20,7 +20,7 @@ H_LOWER = ARENA_LEFT + BUFFER_SPACE
 
 # Boid parameters
 MAX_FORWARD_SPEED = float(os.getenv('MAX_FORWARD_SPEED', "0.50"))       # m /s 
-MAX_ANGULAR_SPEED = float(os.getenv('MAX_ANGULAR_SPEED', "50.0"))       # m /s 
+MAX_ANGULAR_SPEED = float(os.getenv('MAX_ANGULAR_SPEED', "20.0"))       # m /s 
 SEPARATION_DISTANCE = float(os.getenv('SEPARATION_DISTANCE', "0.20"))   # m
 VIEW_DISTANCE = float(os.getenv('VIEW_DISTANCE', "0.40"))               # m
 
