ed730bf375f567b980f8dcdcd52ea598610c3dd9
JoshKStudent <jkub0002@student.monash.edu>
2024-05-10 18:15:28 +1000

    Fixed disco mode missing functions

--- ros/src/ebug/ebug/util/DiscoMode.py
+++ ros/src/ebug/ebug/util/DiscoMode.py
@@ -10,12 +10,14 @@ ARENA_BOTTOM = float(os.getenv('ARENA_BOTTOM', "-0.70"))        # m
 
 BUFFER_SPACE = float(os.getenv('BORDER_BUFFER', "0.20"))        # m
 
+MAX_ANGULAR_SPEED = float(os.getenv('MAX_ANGULAR_SPEED', "1.00"))       # rads /s 
+
 def party(main_boid, other_boids):
 
     current_angle = pose_angle(main_boid)
 
     linear_velocity = 0
-    angular_velocity = 1
+    angular_velocity = MAX_ANGULAR_SPEED
 
     if 0 <= current_angle < 2.1:
         led_colour = (255, 0, 0)
@@ -26,6 +28,45 @@ def party(main_boid, other_boids):
 
     return (linear_velocity, angular_velocity, led_colour)
 
+class Vec2():
+    def __init__(self, x_ = 0.0, y_ = 0.0):
+        self.x = x_
+        self.y = y_
+        
+    def from_pose(pose):
+        return Vec2(pose.position.x, pose.position.y)
+    
+    def from_angle(radians):
+        return Vec2( math.cos(radians), math.sin(radians) )
+    
+    
+    def sub(self, other):
+        return Vec2(self.x - other.x, self.y - other.y)
+    
+    def add(self, other):
+        return Vec2(self.x + other.x, self.y + other.y)
+    
+    def scale(self, scale):
+        return Vec2(self.x * scale, self.y * scale)
+    
+    def divide(self, denom):
+        if (denom == 0.0):
+            return Vec2(0.0, 0.0)
+        return self.scale(1.0 / denom)
+    
+    def inversed(self):
+        return self.scale(-1.0)
+    
+    def length(self):
+        return math.sqrt((self.x ** 2) + (self.y ** 2))
+    
+    def normalised(self):
+        return self.divide(self.length())
+    
+    def angle(self):
+        return math.atan2(self.x, self.y)
+
+
 # Function to get the Euler Z angle of a boid
 def pose_angle(pose):
     x, y, z, w = pose.orientation.x, pose.orientation.y, pose.orientation.z, pose.orientation.w
@@ -33,6 +74,4 @@ def pose_angle(pose):
     # Calculate yaw/z component from quaternion
     t3 = +2.0 * (w * z + x * y)
     t4 = +1.0 - 2.0 * (y * y + z * z)
-    yaw_z = math.atan2(t3, t4)
-
-    return clamp(yaw_z)
+    return math.atan2(t3, t4)
