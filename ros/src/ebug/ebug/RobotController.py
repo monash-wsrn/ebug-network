de91bcfe0a641f90077e5de640e6a2d3d2669d60
Mark-Diedericks <mark.diedericks830@gmail.com>
2024-05-10 16:33:42 +1000

    Increase EKF rate

--- ros/src/ebug/ebug/RobotController.py
+++ ros/src/ebug/ebug/RobotController.py
@@ -31,7 +31,7 @@ class RobotController(Node):
 
         self.a_star = AStar()
 
-        self.timer = self.create_timer(0.05, self.odom_pose_update)
+        self.timer = self.create_timer(0.04, self.odom_pose_update)
 
         # self.odom_pub = self.create_publisher(Odometry, 'odometry', 10)
         self.pose_pub = self.create_publisher(PoseWithCovarianceStamped, 'pose_odom', 10)
