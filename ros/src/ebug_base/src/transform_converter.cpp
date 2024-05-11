37ec8a540abfb32c1d5cb0a0460a4cc987acd3e1
Mark-Diedericks <mark.diedericks830@gmail.com>
2024-05-12 00:20:39 +1000

    Structured QoS

--- ros/src/ebug_base/src/transform_converter.cpp
+++ ros/src/ebug_base/src/transform_converter.cpp
@@ -6,9 +6,9 @@ namespace ebug
 {
     TransformConverter::TransformConverter(const rclcpp::NodeOptions& options) : Node("TransformConverter", options)
     {
-        m_Publisher = this->create_publisher<geometry_msgs::msg::PoseWithCovarianceStamped>("pose", 1);
+        m_Publisher = this->create_publisher<geometry_msgs::msg::PoseWithCovarianceStamped>("pose", rclcpp::QoS(1).best_effort().durability_volatile());
 
-        m_Subscription = this->create_subscription<tf2_msgs::msg::TFMessage>("tf_detections", 1, std::bind(&TransformConverter::transform_callback, this, std::placeholders::_1));
+        m_Subscription = this->create_subscription<tf2_msgs::msg::TFMessage>("tf_detections", rclcpp::QoS(1).best_effort(), std::bind(&TransformConverter::transform_callback, this, std::placeholders::_1));
 
         m_Covariance = {
             0.1, 0.0, 0.0, 0.0, 0.0, 0.0,
