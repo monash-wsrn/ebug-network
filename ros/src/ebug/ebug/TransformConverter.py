cc3dd54516392137def4cf6690e72bf5337e09ef
Mark-Diedericks <mark.diedericks830@gmail.com>
2024-05-11 20:02:40 +1000

    Change module naming

--- ros/src/ebug/ebug/TransformConverter.py
+++ ros/src/ebug/ebug/TransformConverter.py
@@ -5,7 +5,7 @@ from rclpy.node import Node
 from tf2_msgs.msg import TFMessage
 from geometry_msgs.msg import PoseWithCovarianceStamped, TransformStamped, Transform
 
-import _tf2 as tf2
+import tf2_py as tf2
 
 
 # CAM_0 = (0, 0,     0) RPY Radians (FRONT)
