3e3d494e98736045bfa358bef42f10a1e3410a82
RPHA0004 <55816338+RPHA0004@users.noreply.github.com>
2024-05-16 13:36:37 +1000

    Delete ros/src/build directory

--- ros/src/build/ebug_base/CMakeFiles/ebug_base.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/ebug_base"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/ebug_base.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
