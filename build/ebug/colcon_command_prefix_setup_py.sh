b0e42e53f4ecbc822208c516cec98631b444517f
RPHA0004 <55816338+RPHA0004@users.noreply.github.com>
2024-05-16 14:20:04 +1000

    Delete build directory

--- build/ebug/colcon_command_prefix_setup_py.sh
+++
@@ -1,2 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/laprp/Documents/ebug-network/install/ebug_base/share/ebug_base/package.sh"
--- build/ebug_base/ament_cmake_python/ebug_base/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import find_packages
-from setuptools import setup
-
-setup(
-    name='ebug_base',
-    version='0.0.0',
-    packages=find_packages(
-        include=('ebug_base', 'ebug_base.*')),
-)
