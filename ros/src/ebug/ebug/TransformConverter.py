773ea9393e251267f44f686a19b78a62e1f8db3f
Richie <richiepham0@gmail.com>
2024-11-18 18:39:28 +1100

    WIP: Ekfabsolute now global

--- ros/src/ebug/ebug/TransformConverter.py
+++ ros/src/ebug/ebug/TransformConverter.py
@@ -1,7 +1,7 @@
 
 import rclpy
 from rclpy.node import Node
-
+from tf2_ros import Buffer, TransformListener
 from tf2_msgs.msg import TFMessage
 from geometry_msgs.msg import PoseWithCovarianceStamped, Transform
 
@@ -29,6 +29,8 @@ class TransformConverter(Node):
 
     def __init__(self):
         super().__init__(self.__class__.__name__)
+        self.tf_buffer = Buffer()
+        self.tf_listener = TransformListener(self.tf_buffer, self)
 
         self.subscription = self.create_subscription(TFMessage, 'tf_detections', self.listener_callback, 100)
         self.publisher = self.create_publisher(PoseWithCovarianceStamped, 'pose', 100)
@@ -38,30 +40,48 @@ class TransformConverter(Node):
 
     def listener_callback(self, tf_det:TFMessage):
         for t in tf_det.transforms:
-            cam_id = int(t.header.frame_id[-1])
+            try:
+                # Look up transform from map to tag
+                tag_in_map = self.tf_buffer.lookup_transform(
+                    'map',
+                    t.child_frame_id,  # apriltag_1
+                    rclpy.time.Time())
 
-            distance = t.transform.translation.z + CAM_OFFSET
-            roll, pitch, _ = quat2rpy(t.transform.rotation)
+                cam_id = int(t.header.frame_id[-1])
+                distance = t.transform.translation.z + CAM_OFFSET
+                roll, pitch, _ = quat2rpy(t.transform.rotation)
 
-            orienation = CAM_ROTATION[cam_id] - roll                        # Calculate the final orientation of the actual robot
-            rotation = (math.pi * 2.0) - pitch                              # Calculate the relative rotation of camera to apriltag
+                orientation = CAM_ROTATION[cam_id] - roll
+                rotation = (math.pi * 2.0) - pitch
 
-            msg = PoseWithCovarianceStamped()
-            msg.header = t.header
-            msg.header.frame_id = t.child_frame_id
-            
-            msg.pose.pose.position.x = math.cos(rotation) * distance        # Distance perpindicular to the normal
-            msg.pose.pose.position.y = 0.0
-            msg.pose.pose.position.z = math.sin(rotation) * distance        # Distance along the normal
+                # Robot's position relative to tag
+                robot_x = math.cos(rotation) * distance
+                robot_y = math.sin(rotation) * distance
 
-            qx, qy, qz, qw = rpy2quat(0.0, orienation, 0.0)
-            msg.pose.pose.orientation.x = qx
-            msg.pose.pose.orientation.y = qy
-            msg.pose.pose.orientation.z = qz
-            msg.pose.pose.orientation.w = qw
+                # Tag's position in map
+                tag_x = tag_in_map.transform.translation.x
+                tag_y = tag_in_map.transform.translation.y
 
-            msg.pose.covariance = mat6diag(1e-6 * abs(distance * 10))            # Decrease covariance as the bot moves further away
-            self.publisher.publish(msg)
+                msg = PoseWithCovarianceStamped()
+                msg.header = t.header
+                msg.header.frame_id = "map"
+
+                # Calculate robot's global position
+                msg.pose.pose.position.x = tag_x - robot_x
+                msg.pose.pose.position.y = tag_y - robot_y
+                msg.pose.pose.position.z = 0.0
+
+                qx, qy, qz, qw = rpy2quat(0.0, orientation, 0.0)
+                msg.pose.pose.orientation.x = qx
+                msg.pose.pose.orientation.y = qy
+                msg.pose.pose.orientation.z = qz
+                msg.pose.pose.orientation.w = qw
+
+                msg.pose.covariance = mat6diag(1e-6 * abs(distance * 10))
+                self.publisher.publish(msg)
+
+            except Exception as e:
+                self.get_logger().warn(f'Transform lookup failed: {str(e)}')
 
 
 def mat6diag(v):
--- ros/src/ebug/launch/teleop.launch.py
+++ ros/src/ebug/launch/teleop.launch.py
@@ -1,9 +1,10 @@
 import os
 from launch import LaunchDescription
 from launch.actions import GroupAction
-from launch_ros.actions import Node, ComposableNodeContainer
+from launch_ros.actions import Node, ComposableNodeContainer, LoadComposableNodes
 from launch_ros.descriptions import ComposableNode
 from launch_ros.substitutions import FindPackageShare
+
 
 def create_camera_composable_nodes(ROBOT_ID, CAM_ID, PKG_SHARE, VIDEO_DEVICE):
     FRAME_RATE = 10.0
@@ -19,29 +20,29 @@ def create_camera_composable_nodes(ROBOT_ID, CAM_ID, PKG_SHARE, VIDEO_DEVICE):
             namespace=NAMESPACE,
             parameters=[
                 {'video_device': VIDEO_DEVICE, 'camera_name': CAM_ID,
-                 'camera_info_url': f'file://{CAM_INFO}', 'frame_id': CAM_ID,
+                 'camera_info_url': f'file://{CAM_INFO}', 'frame_id': f'{ROBOT_ID}/{CAM_ID}', 
                  'pixel_format': 'raw_mjpeg', 'framerate': FRAME_RATE,
                  'image_height': HEIGHT, 'image_width': WIDTH}
             ]
         ),
         ComposableNode(
-            package='ebug_base', plugin='ebug::ByteRectifier', 
+            package='ebug_base', plugin='ebug::ByteRectifier',
             name='ByteRectifier', namespace=NAMESPACE
         ),
         ComposableNode(
-            package='ebug_base', plugin='ebug::JpegRepublisher', 
+            package='ebug_base', plugin='ebug::JpegRepublisher',
             name='DecompressMJPEG', namespace=NAMESPACE,
             parameters=[{'in_transport': 'compressed', 'out_transport': 'raw'}],
-            remappings=[('in/compressed', 'image_compressed'), 
+            remappings=[('in/compressed', 'image_compressed'),
                         ('out', 'image_uncompressed')]
         ),
         ComposableNode(
-            package='image_proc', plugin='image_proc::RectifyNode', 
+            package='image_proc', plugin='image_proc::RectifyNode',
             name='RectifyColor', namespace=NAMESPACE,
             remappings=[('image', 'image_uncompressed')]
         ),
         ComposableNode(
-            package='apriltag_ros', plugin='AprilTagNode', 
+            package='apriltag_ros', plugin='AprilTagNode',
             name='AprilTag', namespace=NAMESPACE,
             parameters=[APRIL_TAG_PATH],
             remappings=[('/tf', f'/{ROBOT_ID}/tf_detections')]
@@ -49,18 +50,46 @@ def create_camera_composable_nodes(ROBOT_ID, CAM_ID, PKG_SHARE, VIDEO_DEVICE):
     ]
     return nodes
 
-def create_robot_nodes(ROBOT_ID, PKG_SHARE):
-    return [
+
+def generate_launch_description():
+    ROBOT_ID = os.getenv('ROBOT_ID', "default")
+    CAMERA_IDS = os.getenv('CAMERAS', 'cam_0').split(',')
+    PKG_SHARE = FindPackageShare(package='ebug').find('ebug')
+
+    # Create camera nodes for all cameras
+    COMPOSABLE_NODES = []
+    camera_transforms = []
+    VIDX = 0  # Video device index
+    for cam_id in CAMERA_IDS:
+        COMPOSABLE_NODES.extend(
+            create_camera_composable_nodes(
+                ROBOT_ID,
+                cam_id,
+                PKG_SHARE,
+                f'/dev/video{VIDX}'
+            )
+        )
+        camera_transforms.append(
+            Node(
+                package='tf2_ros',
+                executable='static_transform_publisher',
+                arguments=['0', '0', '0.1', '0', '0', '0', 
+                          f'{ROBOT_ID}', f'{ROBOT_ID}/{cam_id}'],
+                name=f'static_transform_base_to_{cam_id}'
+            )
+        )
+        VIDX += 2
+
+    robot_nodes = [
         Node(
             package='ebug', executable='RobotController',
             name='RobotController', namespace=ROBOT_ID, output='screen'
         ),
         Node(
-            package='ebug',
-            executable='TransformConverter',
-            name='TransformConverter',
-            namespace='ebug03',
-            output='screen'
+            package='ebug', executable='TransformConverter',
+            name='TransformConverter', namespace=ROBOT_ID, output='screen', remappings=[
+                ('tf_detections', f'/{ROBOT_ID}/tf_detections'),
+                ('pose', f'/{ROBOT_ID}/apriltag_pose')]
         ),
         Node(
             package='robot_localization', executable='ekf_node',
@@ -80,25 +109,26 @@ def create_robot_nodes(ROBOT_ID, PKG_SHARE):
                 os.path.join(PKG_SHARE, 'config/ekfAbsolute.yaml'),
                 {"odom_frame": f"{ROBOT_ID}_odom"},
                 {"base_link_frame": f"{ROBOT_ID}"},
-                {"world_frame": "map"},  # Assuming you're using "map" as the world frame
+                {"world_frame": "map"},
             ],
             remappings=[
                 ('odometry/filtered', 'ekf_absolute'),
-                ('/tf', '/ebug03/tf_detections'),
-                ('pose0', '/ebug03/apriltag_pose')
+                ('/tf', f'/{ROBOT_ID}/tf_detections'),
+                ('odom0', f'/{ROBOT_ID}/ekf_relative'),  # Use ekf_relative as odom input
+                ('pose0', f'/{ROBOT_ID}/apriltag_pose')  # Use AprilTag pose as pose input
             ]
-        )
+        ),
+        # Add a static transform between map and ebug03/odom
+        Node(
+            package='tf2_ros',
+            executable='static_transform_publisher',
+            arguments=['0', '0', '0', '0', '0', '0', 'map', f'{ROBOT_ID}_odom'],  # Adjust offset if necessary
+            name='static_transform_map_to_odom'
+        ), 
     ]
-
-def generate_launch_description():
-    ROBOT_ID = os.getenv('ROBOT_ID', "default")
-    CAM_ID = 'cam_1'
-    PKG_SHARE = FindPackageShare(package='ebug').find('ebug')
-    VIDEO_DEVICE = '/dev/video2'
-
-    camera_nodes = create_camera_composable_nodes(ROBOT_ID, CAM_ID, PKG_SHARE, VIDEO_DEVICE)
-    robot_nodes = create_robot_nodes(ROBOT_ID, PKG_SHARE)
-
+    
+    robot_nodes.extend(camera_transforms)
+    
     return LaunchDescription([
         GroupAction(
             actions=[
@@ -107,7 +137,7 @@ def generate_launch_description():
                     namespace='',
                     package='rclcpp_components',
                     executable='component_container',
-                    composable_node_descriptions=camera_nodes,
+                    composable_node_descriptions=COMPOSABLE_NODES,
                     output='screen',
                 ),
                 *robot_nodes
